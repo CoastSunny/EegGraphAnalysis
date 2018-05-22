4edcbd9c8f790653536e52f427a28483359f5c60
Davi93 <davide.aloi93@gmail.com>
2018-05-22 18:06:43 +0200

    connectivity strength fixed

--- connectivity_strength.py
+++ connectivity_strength.py
@@ -28,49 +28,49 @@ p_picks = np.append(lp_picks,rp_picks) #parietal
 
 #Connectivity strength for temporal electrodes
 t_con = r_con[t_picks][:, t_picks]
-t_con_mean = round(np.mean(t_con),4)
+t_con_mean = round(np.nanmean(t_con),4)
 #Connectivity strength for frontal electrodes
 f_con = r_con[f_picks][:, f_picks]
-f_con_mean = round(np.mean(f_con),4)
+f_con_mean = round(np.nanmean(f_con),4)
 #Connectivity strength for occipital electrodes
 o_con = r_con[o_picks][:, o_picks]
-o_con_mean = round(np.mean(o_con),4)
+o_con_mean = round(np.nanmean(o_con),4)
 #Connectivity strength for parietal electrodes
 p_con = r_con[p_picks][:, p_picks]
-p_con_mean = round(np.mean(p_con),4)
+p_con_mean = round(np.nanmean(p_con),4)
 
 #Now we consider left/right electrodes, we will therefore have 8 different means
 #Left/right temporal electrodes
 #Left temporal
 lt_con = r_con[lt_picks][:, lt_picks]
-lt_con_mean = round(np.mean(lt_con),4)
+lt_con_mean = round(np.nanmean(lt_con),4)
 #right temporal
 rt_con = r_con[rt_picks][:, rt_picks]
-rt_con_mean = round(np.mean(rt_con),4)
+rt_con_mean = round(np.nanmean(rt_con),4)
 
 #Left/right frontal electrodes
 #Left frontal
 lf_con = r_con[lf_picks][:, lf_picks]
-lf_con_mean = round(np.mean(lf_con),4)
+lf_con_mean = round(np.nanmean(lf_con),4)
 #right frontal
 rf_con = r_con[rf_picks][:, rf_picks]
-rf_con_mean = round(np.mean(rf_con),4)
+rf_con_mean = round(np.nanmean(rf_con),4)
 
 #Left/right parietal electrodes
 #Left parietal
 lp_con = r_con[lp_picks][:, lp_picks]
-lp_con_mean = round(np.mean(lp_con),4)
+lp_con_mean = round(np.nanmean(lp_con),4)
 #right parietal
 rp_con = r_con[rp_picks][:, rp_picks]
-rp_con_mean = round(np.mean(rp_con),4)
+rp_con_mean = round(np.nanmean(rp_con),4)
 
 #Left/right occipital electrodes
 #Left occipital
 lo_con = r_con[lo_picks][:, lo_picks]
-lo_con_mean = round(np.mean(lo_con),4)
+lo_con_mean = round(np.nanmean(lo_con),4)
 #right occipital
 ro_con = r_con[ro_picks][:, ro_picks]
-ro_con_mean = round(np.mean(ro_con),4)
+ro_con_mean = round(np.nanmean(ro_con),4)
 
 
 #Plotting the data
